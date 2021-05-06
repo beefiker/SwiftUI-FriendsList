bec56e11b172a2b165c614c0faff268b8ba37761
J young <beefiker@gmail.com>
2021-05-06 20:15:45 +0900

    Initial Commit

---
+++ SwiftUI-FriendsList/SwiftUI_FriendsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FriendsListApp.swift
+//  SwiftUI-FriendsList
+//
+//  Created by bee on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FriendsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
